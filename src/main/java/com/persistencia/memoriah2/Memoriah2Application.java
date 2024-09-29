62b34ad9778ebbf3e75efd5288411a01190b4604
colindresr <colindresr01@gmail.com>
2024-09-29 16:56:49 -0600

    Persistencia en Memoria con H2

---
+++ src/main/java/com/persistencia/memoriah2/Memoriah2Application.java
@@ -0,0 +1,13 @@
+package com.persistencia.memoriah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Memoriah2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Memoriah2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/persistencia/memoriah2/Memoriah2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistencia.memoriah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memoriah2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
